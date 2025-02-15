92148876e9b761f78da315ecf28d6d442cdc13e8
hjkhgkhljA2 <102316962+hjkhgkhljA2@users.noreply.github.com>
2025-02-16 01:07:54 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, render_template, request
 from g4f.client import Client
 
-app = Flask(__name__, template_folder="C:\\Users\\Afshin\\Desktop\\")
-
+app = Flask(__name__)  # به صورت پیش‌فرض Flask به دنبال پوشه‌ی "templates" در همان دایرکتوری است.
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     image_url = None
     if request.method == 'POST':
-        # دریافت ورودی کاربر، با مقدار پیش‌فرض 'a men'
         prompt = request.form.get('prompt', 'a men')
         
-        # ایجاد یک نمونه از Client و ارسال درخواست تولید تصویر
         client = Client()
         response = client.images.generate(
             model="flux",
@@ -19,7 +16,6 @@ def home():
             response_format="url"
         )
         
-        # دریافت URL تصویر از پاسخ API
         image_url = response.data[0].url
 
     return render_template('index.html', image_url=image_url)
